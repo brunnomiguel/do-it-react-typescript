25706636a0c1e4f05bb0c1bc390c3490dcb24aba
Brunno Miguel <miguel.brunno06@gmail.com>
2022-08-16 17:15:44 -0300

    feat: card component with styles using chakra ui

---
+++ src/components/Card/index.tsx
@@ -0,0 +1,62 @@
+import {
+  Box,
+  Center,
+  Flex,
+  Heading,
+  HStack,
+  Progress,
+  Text,
+} from "@chakra-ui/react";
+import { FaCheck, FaTrash } from "react-icons/fa";
+
+export const Card = () => {
+  return (
+    <Box
+      cursor="pointer"
+      borderWidth="1px"
+      borderColor="grey.50"
+      boxShadow="base"
+      padding="7"
+      w={["330px", "auto"]}
+      _hover={{ transform: "translateY(-7px)", borderColor: "grey.100" }}
+      transform="border 0.2s, ease 0s, transform 0.2s"
+    >
+      <Flex justifyContent="space-between">
+        <Heading as="h1" size="md">
+          Studing React Js Styled Components
+        </Heading>
+        <HStack spacing="4">
+          <Center
+            as="button"
+            w="30px"
+            h="30px"
+            borderWidth="1px"
+            borderRadius="5px"
+            borderColor="grey.200"
+            bgColor="white"
+          >
+            <FaTrash color="grey.200" />
+          </Center>
+          <Center
+            as="button"
+            w="30px"
+            h="30px"
+            borderWidth="1px"
+            borderRadius="5px"
+            borderColor="grey.200"
+            bgColor="white"
+          >
+            <FaCheck color="grey.200" />
+          </Center>
+        </HStack>
+      </Flex>
+      <Box w="100%" mt="4">
+        <Text>Estudar styled components meia hora por dia</Text>
+        <Progress colorScheme="purple" mt="2.5" value={10} />
+        <Text color="grey.200" mt="3">
+          16 de agosto de 2022
+        </Text>
+      </Box>
+    </Box>
+  );
+};
--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,4 +1,5 @@
-import { Box } from "@chakra-ui/react";
+import { Box, Grid } from "@chakra-ui/react";
+import { Card } from "../../components/Card";
 import { SearchBox } from "../../components/Form/SearchBox";
 import { Header } from "../../components/Header";
 
@@ -7,6 +8,17 @@ export const Dashboard = () => {
     <Box>
       <Header />
       <SearchBox />
+      <Grid
+        w="100%"
+        templateColumns="repeat(auto-fill, minmax(420px, 1fr))"
+        gap="10"
+        paddingX="8"
+        mt="8"
+      >
+        {[1, 2, 3, 4, 5, 6].map((_) => (
+          <Card />
+        ))}
+      </Grid>
     </Box>
   );
 };
